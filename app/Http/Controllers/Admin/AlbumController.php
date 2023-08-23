cc72216641558c98086adaa52dba1c804fc8299b
Patrickhell <patrickhell19@yahoo.fr>
2023-08-23 19:50:08 +0200

    implementazione delle rotte edit e update

--- app/Http/Controllers/Admin/AlbumController.php
+++ app/Http/Controllers/Admin/AlbumController.php
@@ -7,6 +7,7 @@ use App\Models\Album;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 
 class AlbumController extends Controller
 {
@@ -58,17 +59,34 @@ class AlbumController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Album $album)
     {
-        //
+        return view('admin.albums.edit', compact('album'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Album $album) //usiamo dipendencies injection sostituendo string $id 
     {
-        //
+        $data = $request->validate([
+            'singer_name' => ['required', 'min:10', 'max:255'],
+            //per risolvere il problema dell'alert che dice che il titolo è già stato utilizzato infatti perché esendo unico non pùo essere usato più di una volta
+            // si usa : il methodo ignore() usando la libreria: use Illuminate\Validation\Rule;  
+            'title' => ['required', Rule::unique('albums')->ignore($album->id), 'max:255'],
+            'imageUrl' => ['url:https'],
+            'genres' => ['required', 'max:255'],
+            'songs_number' => ['required', 'max:20'],
+
+        ]);
+
+        // non potendo usare un methodo statico essendo che si pùo modificare il singolo campo del form, non si può scrivere: $album::update
+        //invece di compilare tutto a mano, e salvare, usiamo le fillable
+
+        $data['slug'] = Str::of($data['title'])->slug('-');
+
+        $album->update($data);
+        return redirect()->route('admin.albums.show', compact('album'));
     }
 
     /**
